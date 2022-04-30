8dc599a11970fdb3eff0e8ecddb3416da4d2b80b
henzonz <zhenzon26@gmail.com>
2022-04-30 06:24:48 -0700

    Testing different routing methods for github pages

--- kusinanichichi/application/frontend/src/App.js
+++ kusinanichichi/application/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import SilogMeals from './FoodMenu/SilogMeals'
 import Home from './Home'
 import Navbar from './Navbar'
@@ -13,7 +13,7 @@ import Wings from './FoodMenu/Wings'
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         
         <Switch>
@@ -23,7 +23,7 @@ function App() {
           <Route path="/drinks">
             <Drinks />
           </Route>
-          <Route path="/Kusinanichichi/silogmeals">
+          <Route path="/silogmeals">
             <SilogMeals />
           </Route>
           <Route path="/wings">
@@ -36,7 +36,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
